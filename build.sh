4f068e7d5fa7bffa14f7578c9be4058ccc96d006
Евгений Матвеев <159659209+mmTvv@users.noreply.github.com>
2024-08-23 21:35:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/PaperMC/Folia
+git clone https://github.com/PaperMC/Folia -b dev/1.21.1
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
